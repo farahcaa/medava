1450691dcd11a1dbec6ecdd1b9dbe17681f8d8f1
Chris Farah <farahca@mail.uc.edu>
2024-07-30 14:25:49 -0400

    Fin checkpoint 4

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,26 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
+public abstract class Medicine {
+    private String mMedicineName;
+    public Medicine(String medicineName) {
+        mMedicineName = medicineName;
+    }
+    public String getMedicineName() {
+        return mMedicineName;
+      }
+          public abstract MedicineSchedule getSchedule();
+    public double minimumTemperature() {
+        return 0.0;
+    }
 
+    public double maximumTemperature() {
+        return 100.0;
+    }
+      public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+                highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,38 @@
 package edu.uc.cs3003.medava;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transporter {
+    private double mLowTemperature, mHighTemperature;
+    private List<Medicine> goods; 
+    private String mTransporterName;
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+    {
+         goods = new ArrayList<Medicine>(); 
+    }
+    public void ship() {
+        // Do some shipping!
+    }
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
 }
