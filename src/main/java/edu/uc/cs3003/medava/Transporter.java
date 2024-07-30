f8667f263afa993ecc98d801bf49a342d8456963
Chris Farah <farahca@mail.uc.edu>
2024-07-30 16:34:11 -0400

    done

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -7,40 +7,50 @@ import java.util.List;
 
 public class Transporter {
     private double mLowTemperature, mHighTemperature;
-    private List<Object> goods; 
+    private List<Shippable> goods; 
     private String mTransporterName;
     public Transporter(String transporterName, double lowTemp, double highTemp) {
         mTransporterName = transporterName;
         mLowTemperature = lowTemp;
         mHighTemperature = highTemp;
     }
-    public boolean load(Object itemToLoad) {
-        try {
-            Method isTemperatureRangeAcceptableMethod = itemToLoad.getClass().getMethod("isTemperatureRangeAcceptable",
-                    Double.class, Double.class);
-            boolean resultOfMethodCall = (boolean) isTemperatureRangeAcceptableMethod.invoke(itemToLoad,
-                    Double.valueOf(mLowTemperature), Double.valueOf(mHighTemperature));
-            if (resultOfMethodCall) {
-                goods.add(itemToLoad);
-            }
-            return resultOfMethodCall;
-        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
-                | InvocationTargetException e) {
-            return false;
+    // public boolean load(Object itemToLoad) {
+    //     try {
+    //         Method isTemperatureRangeAcceptableMethod = itemToLoad.getClass().getMethod("isTemperatureRangeAcceptable",
+    //                 Double.class, Double.class);
+    //         boolean resultOfMethodCall = (boolean) isTemperatureRangeAcceptableMethod.invoke(itemToLoad,
+    //                 Double.valueOf(mLowTemperature), Double.valueOf(mHighTemperature));
+    //         if (resultOfMethodCall) {
+    //             goods.add(itemToLoad);
+    //         }
+    //         return resultOfMethodCall;
+    //     } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+    //             | InvocationTargetException e) {
+    //         return false;
+    //     }
+    // }
+        public Shippable unload() {
+        return goods.remove(0);
+    }
+
+    public boolean load(Shippable itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            return goods.add(itemToLoad);
         }
+        return false;
     }
     public String getTransporterName() {
         return mTransporterName;
     }
     {
-         goods = new ArrayList<Object>(); 
+         goods = new ArrayList<Shippable>(); 
     }
     public void ship() {
         // Do some shipping!
     }
-    public Object unload() {
-        return goods.remove(0);
-    }
+    // public Object unload() {
+    //     return goods.remove(0);
+    // }
     public boolean isEmpty() {
         return goods.isEmpty();
     }
